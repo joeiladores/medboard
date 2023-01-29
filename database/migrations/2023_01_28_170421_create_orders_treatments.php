e201a7b8187664bdafc37759a4d39ad6fcf03c3c
joeiladores <joelynladores@gmail.com>
2023-01-30 00:19:38 +0800

    Please check if this is the correct orders treatment

--- database/migrations/2023_01_28_170421_create_orders_treatments.php
+++ database/migrations/2023_01_28_170421_create_orders_treatments.php
@@ -16,23 +16,6 @@ return new class extends Migration
         Schema::create('order_treatments', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-
-            $table->enum('usertype', ['Admin', 'Doctor', 'Nurse', 'Chief Nurse'])->default('Admin')->nullable();
-            $table->string('lastname')->nullable();
-            $table->string('firstname')->nullable();
-            $table->string('middlename')->nullable();
-            $table->enum('gender', ['Male', 'Female'])->nullable();
-            $table->string('address')->nullable();
-            $table->string('phone')->nullable();
-            $table->string('department')->nullable();
-            $table->string('specialization')->nullable();
-            $table->string('imagepath')->nullable()->nullable();
-            $table->enum('status', ['Active', 'Inactive'])->nullable();
-
-            $table->timestamp('email_verified_at')->nullable();            
-            $table->rememberToken();
             $table->string('type');
             $table->string('instruction');
             $table->date('date_started');
