e4f8688cf5482c574bf6d598e9f6b94ac99e8c89
Oman24everDev <oman24ever@gmail.com>
2023-01-30 21:56:13 +0800

    update route

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,10 @@
 <?php
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
-use App\Http\Controllers\PatientsController;
-use App\Http\Controllers\HomeController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-=======
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\OrderMedicationController;
 use App\Http\Controllers\OrderTransfusionController;
 use App\Http\Controllers\OrderTreatmentController;
->>>>>>> 511a5d6f3a355c45ddcdf161693cba3cf26a6a1e
 
 Route::get('/', function (){
     return redirect()->route('login');
@@ -28,25 +12,6 @@ Route::get('/', function (){
 
 Auth::routes();
 
-<<<<<<< HEAD
-// ------- Admin Panel ------- //
-Route::get('/home', [HomeController::class, 'index'])->name('adminHome');
-
-Route::get('/patients', [HomeController::class, 'patients'])->name('patientView');
-
-
-
-
-// Route::get('/dashboard', function(){
-//     return view('dashboard');
-// });
-
-Route::get('/dashboard', [PatientsController::class, 'displayAll'])->name('dashboard');
-
-Route::post('/createpatient', [PatientsController::class, 'create'])->name('createpatient');
-
-// Route::get('/patient', [App\Http\Controllers\PatientsController::class, 'patient'])->name('patient');
-=======
 Route::get('/dashboard', function(){
     return view('dashboard');
 });
@@ -101,4 +66,3 @@ Route::get('/destroyTreatment/{id}', [OrderTreatmentController::class, 'destroy'
 
 
 
->>>>>>> 511a5d6f3a355c45ddcdf161693cba3cf26a6a1e
