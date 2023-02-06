915435d391d9a0630fd3b030488bf24e93db67bf
ricaguno <gunorica@yahoo.com>
2023-02-07 01:03:07 +0800

    Adjusted controllers to connect with other table

--- app/Http/Controllers/OrderMedicationController.php
+++ app/Http/Controllers/OrderMedicationController.php
@@ -15,7 +15,6 @@ class OrderMedicationController extends Controller
     public function index($id)
 {
     $doctor_order = DoctorOrder::findOrFail ($id);
-    // $doctor_order = DoctorOrder::findOrFail($id);
     $order_medications = OrderMedication::where('doctor_order_id', $doctor_order->id)->orderBy('created_at', 'desc')->get();
     $order_transfusions = OrderTransfusion::where('doctor_order_id', $doctor_order->id)->orderBy('created_at', 'desc')->get();
     $order_treatments = OrderTreatment::where('doctor_order_id', $doctor_order->id)->orderBy('created_at', 'desc')->get();
@@ -24,10 +23,9 @@ class OrderMedicationController extends Controller
     return view('orders', compact('doctor_order', 'order_medications','order_transfusions','order_treatments','progress_notes'));
 }
 
-  
 public function edit($id)
 {
-    $orders_medications = OrderMedication::findOrFail ($id);
+    $orders_medications = OrderMedication::findOrFail($id);
     $doctor_order_id = $orders_medications->doctor_order_id;
 
     return view('editMedication', ['id' => $doctor_order_id])->with('order_medication', $orders_medications);
@@ -39,6 +37,7 @@ public function edit($id)
 
         $doctor_order_id                    = $request->input('doctor_order_id');
         $orders_medication->doctor_order_id = $doctor_order_id;
+        
         $orders_medication->medication      = $request->medication;
         $orders_medication->dose            = $request->dose;
         $orders_medication->quantity        = $request->quantity;
@@ -78,6 +77,5 @@ public function edit($id)
 
         return redirect()->route('orders', ['id' => $doctor_order_id]);
     }
-
     
 }
--- app/Http/Controllers/OrderTransfusionController.php
+++ app/Http/Controllers/OrderTransfusionController.php
@@ -18,10 +18,12 @@ class OrderTransfusionController extends Controller
     {  
         $orders_transfusion = new OrderTransfusion;
 
-        $doctor_order_id                    = $request->input('doctor_order_id');
-        $orders_transfusion->type           = $request->type;
-        $orders_transfusion->fluid_name     = $request->fluid_name;
-        $orders_transfusion->instruction    = $request->instruction;
+        $doctor_order_id                        = $request->input('doctor_order_id');
+        $orders_transfusion->doctor_order_id    = $doctor_order_id;
+        
+        $orders_transfusion->type               = $request->type;
+        $orders_transfusion->fluid_name         = $request->fluid_name;
+        $orders_transfusion->instruction        = $request->instruction;
       
         $orders_transfusion->save();
 
