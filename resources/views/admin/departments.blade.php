abffa264d4968464ca14253d1b41b2a26f5cc67c
joeiladores <joelynladores@gmail.com>
2023-02-07 01:26:28 +0800

    Added department mvc, routes, Updated bed status default value from empty to vacant

---
+++ resources/views/admin/departments.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.adminlayout')
+
+@section('content')
+
+<div class="container">
+  <div class="row">
+    <form method="POST" action="{{ store }}">
+  
+    </form>
+    
+  </div>
+</div>
+
+
+@endsection
