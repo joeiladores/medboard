54ff306557f3716e3e4aca23a8fd1811e80e1547
ricaguno <gunorica@yahoo.com>
2023-02-09 01:01:30 +0800

    Patients view

--- resources/views/nursePatients.blade.php
+++ resources/views/nursePatients.blade.php
@@ -15,18 +15,13 @@ body {
     background: linear-gradient(180deg, rgba(66, 100, 208, 0.7) 0%, #4264D0 100%);
     border-radius:30px;
 }
-#DashboardText{
-    padding:4%;
-}
-#DashboardNurseImg{
-    width: 30%;
-    height: 115%;
-}
+
+
 
 /* Responsiveness */
 @media only screen and (max-width: 1000px) {
     #DashboardCard{
-    height:30%;
+    height:10%;
 }
   #DashboardSide {
     display: none;
@@ -36,11 +31,7 @@ body {
   #DashboardSide {
     display: none;
   }
-  #DashboardNurseImg{
-    width: 45%;
-    height: 90%;
-    margin: 2px;
-  }
+
 }
 @media only screen and (max-width: 660px) {
     #DashboardCard{
@@ -49,15 +40,11 @@ body {
   #DashboardSide {
     display: none;
   }
-  #DashboardNurseImg{
-    width: 45%;
-    height: 90%;
-    margin: 2px;
-  }
+ 
 }
 @media only screen and (max-width: 500px) {
     #DashboardCard{
-    height:20%;
+    height:22%;
 }
 #DashboardText{
     font-size:15px;
@@ -66,10 +53,7 @@ body {
   #DashboardSide {
     display: none;
   }
-  #DashboardNurseImg{
-    width: 40%;
-    height: 85%;
-  }
+
 }
 
 
@@ -78,22 +62,14 @@ body {
 
 <div class="row">
   <div class="col-lg-10">
-    <div class="card shadow" id="DashboardCard">
-        <h4 id="DashboardText" class="text-light" style="position: absolute;">Good Day, Nurse "Name here"<br><br>
-        <p style="font-size:15px;">Today, you are assigned to station "here"<br>
-        Have a nice day at work!</p>
-        </h4>
-    <!-- Nurse Dashboard img -->
-    <div style="text-align:right;">
-        <img id="DashboardNurseImg" src="{{ asset('images/nursedashboard.png') }}" alt="Image">
-    </div>
+    <div class="card shadow" id="DashboardCard" style="display:none;">
+        
 </div>
-
-
+<h4>Patients List</h4>
     <div class="row">
     <div class="col-lg-12">
       <div class="card rounded shadow mt-3 p-2">
-      <table class="table" id="nursesDashboardTable">
+      <table class="table p-4" id="nursesDashboardTable">
             <thead>
                 <tr>
                     <th>Date</th>
@@ -161,6 +137,20 @@ body {
                     <td><a href="#">🗒</a></td>
                     <td>Remarks</td>
                 </tr>
+                <tr>
+                    <td>Jan 2,2022</td>
+                    <td>Sample Name</td>
+                    <td>A01</td>
+                    <td><a href="#">🗒</a></td>
+                    <td>Remarks</td>
+                </tr>
+                <tr>
+                    <td>Jan 2,2022</td>
+                    <td>Sample Name</td>
+                    <td>A01</td>
+                    <td><a href="#">🗒</a></td>
+                    <td>Remarks</td>
+                </tr>
             </tbody>
            </table>
       </div>
@@ -169,7 +159,7 @@ body {
   </div>
   <div class="col-lg-2" id="DashboardSide">
     <!-- Side Profile -->
-    <div style="padding:20%; height:500px; background: linear-gradient(180deg, #4264D0 0%, rgba(66, 100, 208, 0.84532) 18.86%, rgba(66, 100, 208, 0.32) 100%);box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.25);border-radius: 30px 30px 0px 0px;">
+    <div style="padding:20%; height:530px; background: linear-gradient(180deg, #4264D0 0%, rgba(66, 100, 208, 0.84532) 18.86%, rgba(66, 100, 208, 0.32) 100%);box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.25);border-radius: 30px 30px 0px 0px;">
         <center>
           <img class="mb-2" src="{{ asset('images/nurseprofile.png') }}" alt="Image" style="width: 100%; height: 30%;">
           <p style="font-size:17px; font-weight:600;" class="text-light">Nurse Cruz</p>
@@ -211,7 +201,7 @@ body {
 <script>
   $(document).ready( function () {
     $('#nursesDashboardTable').DataTable({
-        "pageLength": 5,
+        "pageLength": 10,
         "lengthChange": false
     });
 } );  
