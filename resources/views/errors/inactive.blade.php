a9c9cbb5dd9c3e657d76a91fd24ccb4fa7ea03b2
joeiladores <joelynladores@gmail.com>
2023-02-14 12:39:49 +0800

    Added back to home button for unauthorized page

--- resources/views/errors/inactive.blade.php
+++ resources/views/errors/inactive.blade.php
@@ -1,5 +1,5 @@
 @extends('errors::displayerrorinactive')
 
 @section('title', __('Inactive'))
-@section('code', 'Inactive')
+@section('code', '401')
 @section('message', __('Your account is not active. Please contact the admin'))
