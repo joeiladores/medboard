7a5fd2b229f55150ae2ec4637e6cee887aa9de76
Oman24everDev <oman24ever@gmail.com>
2023-02-14 23:03:45 +0800

    change of doctordashboard images

--- app/Http/Controllers/DoctorDashboardController.php
+++ app/Http/Controllers/DoctorDashboardController.php
@@ -6,6 +6,7 @@ use App\Models\Patient;
 use App\Models\User;
 use App\Models\Bed;
 use App\Models\AdmissionNew;
+use App\Models\DoctorOrder;
 use Illuminate\Http\Request;
 
 class DoctorDashboardController extends Controller
@@ -13,13 +14,15 @@ class DoctorDashboardController extends Controller
     public function index(){
 
         // $totalPatients      = Patient::count();
+        $totalDoctorOrders  = DoctorOrder::count();
+        $totalPatients      = Patient::count();
+        $totalDoctors       = User::where('usertype', 'doctor')
+                    ->where('status', 'active')  ->count();
+        $totalNurses        = User::where('usertype', 'nurse')
+                    ->orWhere('usertype', 'chiefnurse')->where('status', 'active')->count();
+        $totalVacantBeds    = Bed::where('status', 'vacant')->count();
 
-        $totalPatients  = Patient::count();
-        $totalDoctors   = User::where('usertype', 'doctor')->where('status', 'active')->count();
-        $totalNurses    = User::where('usertype', 'nurse')->orWhere('usertype', 'chiefnurse')->where('status', 'active')->count();
-        $totalVacantBeds = Bed::where('status', 'vacant')->count();
-
-        return view('doctorHome', compact('totalPatients', 'totalDoctors', 'totalNurses', 'totalVacantBeds'));
+        return view('doctorHome', compact('totalPatients', 'totalDoctors', 'totalNurses', 'totalVacantBeds', 'totalDoctorOrders'));
 
     }
 }
--- resources/views/doctorHome.blade.php
+++ resources/views/doctorHome.blade.php
@@ -2,18 +2,19 @@
 @section('content')
 <!-- Doctor Dashboard -->
 <div class="container-fluid px-4 mt-3">
-    <div class="row">
-        <div class="col-md-12">
+    <div class="row d-flex justify-content-center">
+        <div class="col-md-11">
             <div class="row card-bg border shadow rounded-5 second-text mt-3 py-4 ps-4">
-                <div class="col-6 col-lg-4 col-sm-12">
-                    <h2>Welcome! <br> Dr. {{ Auth::user()->firstname . " " . Auth::user()->lastname }}</h2>
-                    <p>Have a nice day!</p>
+                <div class="col-6 col-lg-4 col-sm-12 ps-4">
+                    <h2>Welcome!</h2>
+                    <p class="fs-1 fw-bold">Dr. {{ Auth::user()->firstname . " " . Auth::user()->lastname }}</p> 
+                    <p class="fs-4">Have a nice day!</p>
                     <br>
-                    <br>
-                    <div>Date: <span>{{ \Carbon\Carbon::now(new DateTimeZone('Asia/Singapore'))->format('D, F j, Y') }}</span></div>
+                    <div class="fs-4">Date: <span class="fs-4">{{ \Carbon\Carbon::now(new DateTimeZone('Asia/Singapore'))->format('D, F j, Y') }}</span></div>
                 </div>
-                <div class="col-6 col-lg-8 pb-3">
-                    <marquee behavior="" direction=""><img class="doc-icon" src="{{asset('/images/doctor-icon.png')}}" alt=""></marquee>
+                <div class="col-6 col-lg-8 text-center">
+                    <img class="doc-icon img-fluid" src="{{asset('/images/doctor-icon2.png')}}" alt="">
+                    <img class="doc-icon img-fluid" src="{{asset('/images/doctor-icon3.png')}}" alt="">
                 </div>
             </div>
         </div>
@@ -22,7 +23,7 @@
             <div class="col-md-3 mt-2">
                 <div class="card card-body card-bg text-center border shadow rounded-5">
 
-                    <h3 class="fs-3 fw-bold second-text">{{ $totalPatients }}</h3>
+                    <h3 class="fs-2 fw-bold second-text">{{ $totalPatients }}</h3>
                     <p class="text-primary fs-4">Total Current Patients</p>
 
                     <i class="fa-solid fa-clipboard-list fs-1 primary-text"></i>
@@ -32,8 +33,8 @@
             <div class="col-md-3  mt-2">
                 <div class="card card-body card-bg text-center border shadow rounded-5">
 
-                    <h3 class="fs-3 fw-bold second-text">{{ $totalDoctors }}</h3>
-                    <p class="text-primary fs-4">Total Doctors Active</p>
+                    <h3 class="fs-2 fw-bold second-text">{{ $totalDoctors }}</h3>
+                    <p class="text-primary fs-4">Active Doctors</p>
 
                     <i class="fa-solid fa-user-doctor fs-1 primary-text"></i>
                 </div>
@@ -41,8 +42,8 @@
             <div class="col-md-3  mt-2">
                 <div class="card card-body card-bg text-center border shadow rounded-5">
 
-                    <h3 class="fs-3 fw-bold second-text">---</h3>
-                    <p class="text-primary fs-4">Total Orders</p>
+                    <h3 class="fs-2 fw-bold second-text">{{ $totalDoctorOrders }}</h3>
+                    <p class="text-primary fs-4">Total Doctor Orders</p>
 
                     <i class="fa-solid fa-clipboard-list fs-1 primary-text"></i>
                 </div>
@@ -51,8 +52,8 @@
             <div class="col-md-3  mt-2">
                 <div class="card card-body card-bg text-center border shadow rounded-5">
 
-                    <h3 class="fs-3 fw-bold second-text">{{ $totalNurses }}</h3>
-                    <p class="text-primary fs-4">Total Current Nurses</p>
+                    <h3 class="fs-2 fw-bold second-text">{{ $totalNurses }}</h3>
+                    <p class="text-primary fs-4">Active Nurses</p>
 
                     <i class="fa-solid fa-user-nurse fs-1 primary-text"></i>
                 </div>
